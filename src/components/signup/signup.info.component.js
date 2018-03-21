ecd1e24abece01b1316eca8770fa08234ee81f16
ermondel <ermondel@gmail.com>
2018-03-21 19:18:06 +0200

    Add style to signup info component

--- src/components/signup/signup.info.component.js
+++ src/components/signup/signup.info.component.js
@@ -1,11 +1,12 @@
 /**
  * Signup Info Component
- * version 0.2
+ * version 0.52
  * props
  *  waiting boolean
  *  result boolean
  */
 import Component from '../../component';
+import img_waiting from '../../style/waiting.gif';
 
 class SignupInfo extends Component {
     constructor(props) {
@@ -18,9 +19,23 @@ class SignupInfo extends Component {
     render() {
         const { waiting, result } = this.props;
 
-        if (waiting) return ':: waiting ::';
-        if (!result) return ':: server is not available ::';
-        if (result) return ':: registration completed successfully ::';
+        if (waiting) return `<img src="${img_waiting}" alt="waiting">`;
+
+        if (!result) return `
+        <div id="info" class="failed">
+            <div id="info_inner">
+                <p>Server is not available.</p>
+                <p>Try again or contact <a href="mailto:ermondel@gmail.com" title="support">support</a>.</p>
+            </div>
+        </div>`;
+
+        if (result) return `
+        <div id="info" class="successful">
+            <div id="info_inner">
+                <p>Registration completed successfully.</p>
+                <p>Now you can <a href="#/signin" title="sign in">sign in</a>.</p>
+            </div>
+        </div>`;
     }
 }
 
